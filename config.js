1e29c1cfb5e42ed585b4c80843dfab050f9d015c
Piper <bates.sarah42@gmail.com>
2021-01-24 22:17:10 +0000

    small change to config

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  // consumer_key: "jqrhGlTqrDNc6NNPeOCRQebED",
-  // consumer_secret: "Y6mUQMuD60LQSvqolIebIUIYBL18hluytm5n8ZWxBrPcD1hdRk",
-  // access_token: "1353020896454324224-1EVVqo9BR1xYLOBCI7VyqDtyFA65Sj",
-  // access_token_secret: "nqRKRoAqw9cu8TrvUkPbzfN1EXrt2dFizCf8Wp02iduva"
   consumer_key: process.env.consumer_key,
   consumer_secret: process.env.consumer_secret,
   access_token: process.env.access_token,
